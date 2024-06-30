a07f9e7fa827035fde24d6c8f172ea8575315b62
krishna-iitm <63369847+krishna-iitm@users.noreply.github.com>
2024-06-30 23:16:25 +0530

    add via upload

---
+++ simDocumentation/neddoc/navtreedata.js
@@ -0,0 +1,89 @@
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
+
+var NAVTREE =
+[
+  [`TQDSimModule_final`, `index.html`, [
+    [`Overview`, `index.html`, null],
+    [`Diagrams`, null, [
+      [`Full NED Usage Diagram`, `full-ned-usage-diagram.html`, null],
+      [`Full NED Inheritance Diagram`, `full-ned-inheritance-diagram.html`, null],
+      [`Full MSG Usage Diagram`, `full-msg-usage-diagram.html`, null],
+      [`Full MSG Inheritance Diagram`, `full-msg-inheritance-diagram.html`, null],
+    ] ],
+    [`Packages`, `packages.html`, [
+      [`default`, `packages.html#default`, [
+        [`App`, `App.html`, null],
+        [`BurstyApp`, `BurstyApp.html`, null],
+        [`IApp`, `IApp.html`, null],
+        [`L2Queue`, `L2Queue.html`, null],
+        [`networkTopoInfo`, `networkTopoInfo.html`, null],
+        [`Node`, `Node.html`, null],
+        [`RandomGraph`, `RandomGraph.html`, null],
+        [`Routing`, `Routing.html`, null],
+      ] ],
+      [`ned`, `packages.html#ned`, [
+        [`DatarateChannel`, `ned.DatarateChannel.html`, null],
+        [`DelayChannel`, `ned.DelayChannel.html`, null],
+        [`IBidirectionalChannel`, `ned.IBidirectionalChannel.html`, null],
+        [`IdealChannel`, `ned.IdealChannel.html`, null],
+        [`IUnidirectionalChannel`, `ned.IUnidirectionalChannel.html`, null],
+      ] ],
+    ] ],
+    [`Networks`, null, [
+      [`RandomGraph`, `RandomGraph.html`, null],
+    ] ],
+    [`Modules`, null, [
+      [`App`, `App.html`, null],
+      [`BurstyApp`, `BurstyApp.html`, null],
+      [`L2Queue`, `L2Queue.html`, null],
+      [`networkTopoInfo`, `networkTopoInfo.html`, null],
+      [`Node`, `Node.html`, null],
+      [`Routing`, `Routing.html`, null],
+    ] ],
+    [`Module Interfaces`, null, [
+      [`IApp`, `IApp.html`, null],
+      [`IBidirectionalChannel`, `ned.IBidirectionalChannel.html`, null],
+      [`IUnidirectionalChannel`, `ned.IUnidirectionalChannel.html`, null],
+    ] ],
+    [`Channels`, null, [
+      [`DatarateChannel`, `ned.DatarateChannel.html`, null],
+      [`DelayChannel`, `ned.DelayChannel.html`, null],
+      [`IdealChannel`, `ned.IdealChannel.html`, null],
+    ] ],
+    [`Namespaces`, `namespaces.html`, [
+      [`default`, `namespaces.html#default`, [
+        [`Packet`, `Packet.html`, null],
+      ] ],
+    ] ],
+    [`Message Definitions`, null, [
+      [`Packets`, null, [
+        [`Packet`, `Packet.html`, null],
+      ] ],
+    ] ],
+    [`Files`, null, [
+      [`App.ned`, `App.ned.html`, null],
+      [`BurstyApp.ned`, `BurstyApp.ned.html`, null],
+      [`IApp.ned`, `IApp.ned.html`, null],
+      [`L2Queue.ned`, `L2Queue.ned.html`, null],
+      [`networkTopoInfo.ned`, `networkTopoInfo.ned.html`, null],
+      [`Node.ned`, `Node.ned.html`, null],
+      [`Packet.msg`, `Packet.msg.html`, null],
+      [`RandomGraph.ned`, `RandomGraph.ned.html`, null],
+      [`Routing.ned`, `Routing.ned.html`, null],
+    ] ],
+    [`C++`, null, [
+      [`File List`, `../../doc/doxy/files.html`, null],
+      [`Class List`, `../../doc/doxy/annotated.html`, null],
+      [`Class Hierarchy`, `../../doc/doxy/hierarchy.html`, null],
+      [`Class Members`, `../../doc/doxy/functions.html`, null],
+      [`Namespace List`, `../../doc/doxy/namespaces.html`, null],
+      [`File Members`, `../../doc/doxy/globals.html`, null],
+      [`Namespace Members`, `../../doc/doxy/namespacemembers.html`, null],
+    ] ],
+  ] ],
+];
+
+var NAVTREEINDEX = [
+" ",
+];
